be2fe90f458770a2a21073be6d936b2c5996811c
suryamanipatra <suryamanipatra114@gmail.com>
2024-07-20 20:02:29 +0530

    Connect to blogapp Database

--- api/index.js
+++ api/index.js
@@ -1,6 +1,20 @@
-import express from 'express'
-const app = express();
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 
-app.listen(3000,()=>{
-    console.log('Server is Running on Port 3000');
-})
+const app = express();
+dotenv.config();
+
+console.log(process.env.MONGO_DB_URL); // Add this line to check if the URL is being loaded correctly
+
+mongoose.connect(process.env.MONGO_DB_URL)
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+
+app.listen(3000, () => {
+  console.log('Server is Running on Port 3000');
+});
