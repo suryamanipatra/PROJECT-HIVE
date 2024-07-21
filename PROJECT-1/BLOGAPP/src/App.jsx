3cb66b3aacbe4378f377f91e064874a554b37b0c
suryamanipatra <suryamanipatra114@gmail.com>
2024-07-21 12:12:57 +0530

    Create and Add Footer Component

--- PROJECT-1/BLOGAPP/src/App.jsx
+++ PROJECT-1/BLOGAPP/src/App.jsx
@@ -6,6 +6,7 @@ import Projects from "./pages/Projects"
 import SignIn from "./pages/SignIn"
 import SignUp from "./pages/SignUp"
 import Header from "./components/Header"
+import Footer from "./components/Footer"
 
 
 function App() {    
@@ -22,6 +23,7 @@ function App() {
           <Route path="/signin" element={<SignIn />} /> 
           <Route path="/signup" element={<SignUp />} />
         </Routes>
+        <Footer />
       </BrowserRouter>
     </>
   )
