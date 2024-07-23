2771b1173f586be29e1c0eb19594c9680ae9bbe8
suryamanipatra <suryamanipatra114@gmail.com>
2024-07-23 16:44:56 +0530

    Make the Dashboard Private

--- PROJECT-1/BLOGAPP/src/App.jsx
+++ PROJECT-1/BLOGAPP/src/App.jsx
@@ -7,6 +7,7 @@ import SignIn from "./pages/SignIn"
 import SignUp from "./pages/SignUp"
 import Header from "./components/Header"
 import Footer from "./components/Footer"
+import PrivateRoutes from "./components/PrivateRoutes"
 
 
 function App() {    
@@ -18,7 +19,9 @@ function App() {
         <Routes>
           <Route path="/" element={<Home/>} />
           <Route path="/about" element={<About />} />
-          <Route path="/dashboard" element={<Dashboard />} />
+          <Route element={<PrivateRoutes/>}>
+            <Route path="/dashboard" element={<Dashboard />} />
+          </Route>
           <Route path="/projects" element={<Projects />} />
           <Route path="/signin" element={<SignIn />} /> 
           <Route path="/signup" element={<SignUp />} />
--- PROJECT-1/BLOGAPP/src/components/OAuth.jsx
+++ PROJECT-1/BLOGAPP/src/components/OAuth.jsx
@@ -31,7 +31,7 @@ export default function OAuth() {
                 dispatch(signInSuccess(data));
                 navigate('/')
             }
-        } catch (error) {
+        } catch (error) {   
             console.log(error)
             
         }
