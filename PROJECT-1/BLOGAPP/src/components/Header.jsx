c29a7e2e05c098cb913caa5f4f16456fe0ced6e1
suryamanipatra <suryamanipatra114@gmail.com>
2024-07-23 13:41:09 +0530

    Updating Header Component With User Data

--- PROJECT-1/BLOGAPP/src/components/Header.jsx
+++ PROJECT-1/BLOGAPP/src/components/Header.jsx
@@ -1,13 +1,16 @@
-import { Button, Navbar, TextInput } from 'flowbite-react'
+import { Avatar, Button, Dropdown, Navbar, TextInput } from 'flowbite-react'
 import React from 'react'
 import { Link,useLocation } from 'react-router-dom'
 import {AiOutlineSearch} from 'react-icons/ai'
 import{FaMoon} from 'react-icons/fa'
+import {useSelector} from 'react-redux'
 
 const Header = () => {
-    let path = useLocation().pathname;
+    const path = useLocation().pathname;
+    const {currentUser} = useSelector(state => state.user)
+
   return (
-    <Navbar className='border-b-2'>
+    <Navbar className='border-b-2 mr-8 ml-8'>
     <Link to="/" className='self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white'>
         <span className='px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white'>Surya's</span>Blog
     </Link>
@@ -26,11 +29,32 @@ const Header = () => {
         <Button className='w-12 h-10 hidden sm:inline' color="gray" pill >
             <FaMoon />
         </Button>
+        {currentUser ? (
+            <Dropdown arrowIcon={false} inline label={
+                <Avatar 
+                    alt='user'
+                    img={currentUser.profilePicture}
+                    rounded
+                />
+            }> 
+                <Dropdown.Header>
+                    <span className='block test-sm '>@{currentUser.username}</span>
+                    <span className='block test-sm font-medium truncate'>{currentUser.email}</span>
+                </Dropdown.Header>
+                <Link to={'/dashboard?tab=profile'} >
+                    <Dropdown.Item>Profile</Dropdown.Item>
+                </Link>
+                <Dropdown.Divider/>
+                <Dropdown.Item>Sign Out</Dropdown.Item>
+            </Dropdown>
+        ):(
+
         <Link to='/signin'>
             <Button gradientDuoTone="purpleToBlue" outline>
                 Sign In
             </Button>
         </Link>
+        )}
         <Navbar.Toggle/>
     </div>
     <Navbar.Collapse>
--- PROJECT-1/BLOGAPP/src/pages/SignUp.jsx
+++ PROJECT-1/BLOGAPP/src/pages/SignUp.jsx
@@ -39,7 +39,7 @@ const SignUp = () => {
   }
   return (
     <div className='min-h-screen mt-20' > 
-      <div className='flex p-3 max-x-3xl mx-auto flex-col md:flex-row md:items-center gap-5'>
+      <div className='flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-5'>
         {/* left */}
         <div className='flex-1'>
         <Link to="/" className='font-bold dark:text-white text-4xl'>
